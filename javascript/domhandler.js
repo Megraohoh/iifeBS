cc0f8b129e0ea88185bd6a274da9df5df19ed569
Meghan Debity <mdebity@gmail.com>
2017-03-28 19:59:39 -0500

    reworked layout of code

--- javascript/domhandler.js
+++ javascript/domhandler.js
@@ -2,6 +2,13 @@ var donateBtn = document.getElementById("donate");
 var cancelBtn = document.getElementById("cancel");
 var donorTable = document.getElementById("tableBody");
 
+var donorName = document.getElementById("inputName");
+var donorEmail = document.getElementById("inputEmail");
+var donorDollar = document.getElementById("dollarAmount");
+var donationType = document.getElementById("donationType");
+var donationTypeLump = document.getElementById("donationTypeLump");
+var donationTypePer = document.getElementById("donationTypePer");
+
 
 function clear() {
     inputName.value = "";
@@ -10,16 +17,16 @@ function clear() {
 }
 
 function sendToDom(){
-	donorInfo = Donations.getDonorInfo();
+	donorInput = Donations.getDonorInfo();
 
 	var tableString = "";
-	for(var i=0; i<donorInfo.length; i++){
+	for(var i=0; i<donorInput.length; i++){
 		tableString += `<tr>`;
 		tableString += `<td></td>`;
-		tableString += `<td>${donorInfo[i].inputName}</td>`;
-		tableString += `<td>${donorInfo[i].inputEmail}</td>`;
-		tableString += `<td>${donorInfo[i].dollarAmount}</td>`;
-		tableString += `<td>${donorInfo[i].donationType}</td>`;
+		tableString += `<td>${donorInput[i].inputName}</td>`;
+		tableString += `<td>${donorInput[i].inputEmail}</td>`;
+		tableString += `<td>${donorInput[i].dollarAmount}</td>`;
+		tableString += `<td>${donorInput[i].donationType}</td>`;
 		tableString += `</tr>`;
 
 		donorTable.innerHTML += tableString;
@@ -29,5 +36,27 @@ function sendToDom(){
 
 sendToDom();
 
-donateBtn.addEventListener("click", sendToDom);
 cancelBtn.addEventListener("click", clear);
+// donateBtn.addEventListener("click", function(event) 
+
+function donateAccept(){
+		if (event.target.innerText === "Donate") {
+			newDonor = {}
+			console.log("green");
+			newDonor["Name"] = donorName.value;
+			newDonor["Email"] = donorEmail.value;
+			newDonor["Amount"] = donorDollar.value;
+			newDonor["DonationType"] = donationType;
+		for (var i=0; i<donationType.length; i++) {
+			if (donationTypeLump.checked){
+						newDonor.donationTypeLump = donationType.value;
+					} else if (donationTypePer.checked){
+						newDonor.donationTypePer = donationType.value;
+					}
+		}
+		sendToDom();
+		clear();
+	}
+}
+
+
--- javascript/iifeholder.js
+++ javascript/iifeholder.js
@@ -1,11 +1,5 @@
 var Donations = (function(){		//IIFE
 
-var donorName = document.getElementById("inputName");
-var donorEmail = document.getElementById("inputEmail");
-var donorDollar = document.getElementById("dollarAmount");
-var donationType = document.getElementById("donationType");
-var donationTypeLump = document.getElementById("donationTypeLump");
-var donationTypePer = document.getElementById("donationTypePer");
 
 	var donorInfo = [
 		{
@@ -20,30 +14,12 @@ var donationTypePer = document.getElementById("donationTypePer");
 		getDonorInfo : function(){
 			return donorInfo;
 		},
-		// setDonorObject : function(donorName, donorEmail, donorAmount){
-		// 	donorObject = {}
-		// 	donorObject["Name"] = donorName;
-		// 	donorObject["Email"] = donorEmail;
-		// 	donorObject["Amount"] = donorAmount;
-		// 	donorObject["DonationType"] = donationType;
-		// },
+		
 		setNewDonorObject : function(newDonor){
-			newDonorInfo.push(newDonor);
+			donorInfo.push(newDonor);
+			console.log("donorInfo", donorInfo);
 		},
-		getNewDonorInfo : function(){  
-			var newDonorInfo = {};
-			donorName.inputName = donorName.value;
-			donorEmail.inputEmail = donorEmail.value;
-			donorDollar.dollarAmount = donorDollar.value;
-				if (donationTypeLump.checked){
-					newDonor.donationTypeLump = donationType.value;
-				} else if (donationTypePer.checked){
-					newDonor.donationTypePer = donationType.value;
-				};
 
-			donorTable.setNewDonorObject(newDonor);	
-			
-		}
 	}
 
 })();
